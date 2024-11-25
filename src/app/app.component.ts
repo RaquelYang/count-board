3e735f0046e75fe52a84070fda4a037253366ae4
Raquel <rauqel@pollex.com.tw>
2024-11-25 13:57:01 +0800

    移除 beforeinstallprompt 監聽

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,32 +8,4 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  deferredPrompt: any;
-  // eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-    if (isPlatformBrowser(this.platformId)) {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      window.addEventListener('beforeinstallprompt', (event: any) => {
-        event.preventDefault(); // Prevent the default prompt
-        this.deferredPrompt = event; // Save the event for later use
-      });
-    }
-  }
-
-  addToHomeScreen() {
-    if (this.deferredPrompt) {
-      this.deferredPrompt.prompt(); // 顯示安裝提示
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      this.deferredPrompt.userChoice.then((choiceResult: any) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        this.deferredPrompt = null;
-      });
-    }
-  }
-}
+export class AppComponent {}
