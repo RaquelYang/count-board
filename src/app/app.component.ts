45b870ff0f425ea8ede27bc636bced72f8fa23a9
Raquel <rauqel@pollex.com.tw>
2024-11-27 15:29:29 +0800

    實作計分板區版與邏輯

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { afterNextRender, Component, HostListener, signal} from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
@@ -9,20 +9,63 @@ import { DeviceDetectorService } from 'ngx-device-detector';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  isMobile = false;
-  isLandscape = false;
+  isMobile: boolean;
+  landscape = false;
+  score1 = signal(0);
+  score2 = signal(0);
+  showMinus = false;
 
   constructor(
     deviceService: DeviceDetectorService,
   ) {
     this.isMobile = deviceService.isMobile();
+
+    afterNextRender(() => {
+      const orientationAngle = window.screen.orientation.angle;
+      this.landscape = this.isLandscape(orientationAngle);
+    });
   }
 
   @HostListener('window:orientationchange', ['$event'])
   onOrientationChange(event: Event): void {
     const orientationAngle = (event.target as Window).screen.orientation.angle;
-    this.isLandscape = orientationAngle === 90;
+    this.landscape = this.isLandscape(orientationAngle);
+  }
 
+  isLandscape(deg: number): boolean {
+    return deg === 90;
+  }
+
+  plusScore1(): void {
+    this.score1.update((prev) => prev + 1);
+    this.showMinus = false;
+  }
+
+  minusScore1(e: Event): void {
+    e.stopPropagation();
+    if (this.score1() > 0) {
+      this.score1.update((prev) => prev - 1);
+    }
+    this.showMinus = false;
+  }
+
+  plusScore2(): void {
+    this.score2.update((prev) => prev + 1);
+    this.showMinus = false;
+  }
+
+  minusScore2(e: Event): void {
+    e.stopPropagation();
+    if (this.score2() > 0) {
+      this.score2.update((prev) => prev - 1);
+    }
+    this.showMinus = false;
+  }
+
+  resetScore(): void {
+    this.score1.set(0);
+    this.score2.set(0);
+    this.showMinus = false;
   }
 }
 
