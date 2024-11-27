e9f9810e91f6be59cae822653cd0527627a614ac
Raquel <rauqel@pollex.com.tw>
2024-11-27 16:26:46 +0800

    引入 hammerjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { afterNextRender, Component, HostListener, signal} from '@angular/core';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import Hammer from 'hammerjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [BrowserModule, HammerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -19,6 +21,12 @@ export class AppComponent {
     deviceService: DeviceDetectorService,
   ) {
     this.isMobile = deviceService.isMobile();
+
+    const element = document.getElementById('gestureArea') as HTMLElement;
+    const hammer = new Hammer(element);
+
+    hammer.on('swipe', (event) => console.log('Swipe detected:', event));
+    hammer.on('doubletap', (event) => console.log('Double Tap detected:', event));
 
     afterNextRender(() => {
       const orientationAngle = window.screen.orientation.angle;
